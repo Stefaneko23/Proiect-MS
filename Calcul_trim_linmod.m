b9cbff74b08bfb822e008cf2a24bca0313dea347
Stefaneko23 <127439057+Stefaneko23@users.noreply.github.com>
2024-02-24 12:11:18 +0200

    Add files via upload

---
+++ Calcul_trim_linmod.m
@@ -0,0 +1,25 @@
+[X, U, Y] = trim('Copy_of_SchemaPrincipala1', [], 3, [], [1],[])
+[A1, B1, C1, D1] = linmod('Copy_of_SchemaPrincipala1', X, U, Y)
+
+[X, U, Y] = trim('Copy_of_SchemaPrincipala1', [], 4, [], [1],[])
+[A21, B2, C2, D2] = linmod('Copy_of_SchemaPrincipala1', X, U, Y)
+
+[X, U, Y] = trim('Copy_of_SchemaPrincipala1', [], 5, [], [1],[])
+[A3, B3, C3, D3] = linmod('Copy_of_SchemaPrincipala1', X, U, Y)
+
+[X, U, Y] = trim('Copy_of_SchemaPrincipala1', [], 6, [], [1],[])
+[A4, B4, C4, D4] = linmod('Copy_of_SchemaPrincipala1', X, U, Y)
+
+[X, U, Y] = trim('Copy_of_SchemaPrincipala1', [], 7, [], [1],[])
+[A5, B5, C5, D5] = linmod('Copy_of_SchemaPrincipala1', X, U, Y)
+
+[X, U, Y] = trim('Copy_of_SchemaPrincipala1', [], 8, [], [1],[])
+[A6, B6, C6, D6] = linmod('Copy_of_SchemaPrincipala1', X, U, Y)
+
+[X, U, Y] = trim('Copy_of_SchemaPrincipala1', [], 9, [], [1],[])
+[A7, B7, C7, D7] = linmod('Copy_of_SchemaPrincipala1', X, U, Y)
+
+[X, U, Y] = trim('Copy_of_SchemaPrincipala1', [], 10, [], [1],[])
+[A8, B8, C8, D8] = linmod('Copy_of_SchemaPrincipala1', X, U, Y)
+
+
